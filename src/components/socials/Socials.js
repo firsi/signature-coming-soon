4f601289686c591baf4f3336c5d3bf8818397e43
firsi <firsijc@gmail.com>
2019-12-20 09:10:25 +0000

    fix: landscape orientation not showing

--- src/components/socials/Socials.js
+++ src/components/socials/Socials.js
@@ -2,17 +2,24 @@ import React,{ useState, useEffect } from 'react';
 import './socials.scss';
 
 export const Socials = () => {
-    const [active, setActive] = useState(false);
+    const [active, setActive] = useState(true);
 
-    useEffect(() => {
+    // useEffect(() => {
+        
+        
+    // }, [active])
+
+    const handleClick = () => {
+        setActive(!active)
+        console.log(active)
+        console.log(document.querySelector('.number'))
         active ? document.querySelector('.number').classList.add('number-active') 
             : document.querySelector('.number').classList.remove('number-active') 
-        
-    }, [active])
+    }
 
     return(
         <ul className="social-ul">
-                   <li className="social-link"><a href="#" onTouchStart={() => setActive(!active)} onClick={() => setActive(!active)}>WHATSAPP</a></li>
+                   {/* <li className="social-link"><button href="#" onTouchStart={() => handleClick()} onClick={() => handleClick()}>WHATSAPP</button></li> */}
                    <li className="social-link"><a href="https://www.facebook.com/agencesignature/">FACEBOOK</a></li> 
                    <li className="social-link"><a href="#">YOUTUBE</a></li>
 
